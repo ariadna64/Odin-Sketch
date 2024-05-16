1adb2e372d602846ccb291aaa46ad4f2f1b1b9e3
ariadna64 <157072066+ariadna64@users.noreply.github.com>
2024-05-16 18:45:41 -0300

    Added readme, the basic index and a div with a little style.

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let container = document.createElement("container");
+document.body.appendChild(container);
+
+let div = document.createElement("div");
+container.appendChild(div);
+
+
+for (let i = 0; i = 15; i++) {
+    document.createElement("div");
+    container.appendChild("div");
+
+}
