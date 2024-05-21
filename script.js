48f85ff9c7be3c85174e70883e7f63afc03850da
ariadna64 <157072066+ariadna64@users.noreply.github.com>
2024-05-21 18:44:33 -0300

    Trying to get the hover to work.

--- script.js
+++ script.js
@@ -13,12 +13,20 @@ for (let i = 0; i < 16; i++) {
 }
     
 
-let div2 = document.querySelector("div");
-div2.addEventListener("mouseenter", () => {
-    div2.style.backgroundColor = "red";
-});
+let div2 = document.querySelectorAll("div");
 
+    for (let i = 0; i < div2.length; i++) {
+        evento(div2[i], i);
+    };
+    
+function evento(a,b) {
 
-div2.removeEventListener("mouseenter", () => {
-    div2.style.backgroundColor = "red";
-});
+    div2.addEventListener("mouseenter", () => {
+        div2.style.backgroundColor = "red";
+    });
+
+    div2.addEventListener("mouseleave", () => {
+        div2.style.backgroundColor = "";
+    });
+
+}
