4f99fc4b90b71107bf3ebf401db5382720f644db
ariadna64 <157072066+ariadna64@users.noreply.github.com>
2024-05-23 18:40:14 -0300

    Added text to the button and fixed everything else.

--- script.js
+++ script.js
@@ -2,41 +2,47 @@
 
 let button = document.createElement("button");
 document.body.appendChild(button);
-
+button.textContent = "Squares";
 button.addEventListener("click", () => {
     newGrid = window.prompt("Squares per side?: ");
     
-    const contenedores = document.querySelectorAll("container");
+    if (newGrid < 100) {
+        const contenedores = document.querySelectorAll("container");
     
-    for (const contenedor of contenedores) {
-        contenedor.remove();
-    }
+     for (const contenedor of contenedores) {
+         contenedor.remove();
+        }
 
-    for (let i = 0; i < newGrid; i++) {
-            
-        let container = document.createElement("container");
-        document.body.appendChild(container);
-    
         for (let i = 0; i < newGrid; i++) {
+            
+         let container = document.createElement("container");
+         document.body.appendChild(container);
+    
+            for (let i = 0; i < newGrid; i++) {
     
                 let div = document.createElement("div");
                 container.appendChild(div);
-            }
+             }
     
+        }
+
+        let div2 = document.querySelectorAll("div");
+
+         for (const div of div2) {
+    
+            div.addEventListener("mouseenter", () => {
+                div.style.backgroundColor = "grey";
+            });
+    
+        };
+
     }
-
-    let div2 = document.querySelectorAll("div");
-
-    for (const div of div2) {
     
-        div.addEventListener("mouseenter", () => {
-            div.style.backgroundColor = "grey";
-        });
     
-};
-
 
 });
+
+
 
 // FIRST GRID
 
